77c947bac7e77a7666b4faa8d1d81e8b0a34f52f
Sid <sidnvy@gmail.com>
2023-04-10 23:45:04 +0800

    add collector

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+# First stage: build the ccapi library with Python bindings
+FROM python:3.9-slim-bullseye AS builder
+
+# Install dependencies
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends swig cmake zlib1g-dev libssl-dev build-essential && \
+    rm -rf /var/lib/apt/lists/*
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the entire 'ccapi' directory into the container
+COPY . .
+
+# Prepare the user_specified_cmake_include.cmake file
+COPY binding/user_specified_cmake_include.cmake.example /app/user_specified_cmake_include.cmake
+
+# Uncomment all lines except those containing "FIX" and "EXECUTION_MANAGEMENT"
+RUN sed -i '/FIX\|EXECUTION_MANAGEMENT/!s/#\s*\(define.*\)/\1/' /app/user_specified_cmake_include.cmake
+
+# Build the ccapi library with Python bindings
+RUN mkdir -p binding/build && \
+    cd binding/build && \
+    cmake -DCMAKE_PROJECT_INCLUDE=/app/user_specified_cmake_include.cmake -DBUILD_VERSION=latest -DBUILD_PYTHON=ON -DINSTALL_PYTHON=ON .. && \
+    cmake --build . && \
+    cmake --install .
+
+# Second stage: create the final image with necessary runtime dependencies
+FROM python:3.9-slim-bullseye
+
+# Install necessary runtime dependencies and additional Python packages
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends libssl1.1 && \
+    rm -rf /var/lib/apt/lists/* && \
+    pip install --no-cache-dir python-dateutil pyarrow boto3
+
+# Copy Python bindings and libraries from the builder stage
+COPY --from=builder /usr/local/lib/python3.9/site-packages /usr/local/lib/python3.9/site-packages
+COPY --from=builder /usr/local/lib /usr/local/lib
+
+# Set environment variable for Python bindings
+ENV PYTHONPATH="/usr/local/lib/python3.9/site-packages:${PYTHONPATH}"
+
+# Copy the collector.py script to the container
+COPY collector.py /app/collector.py
+
+# Set the default command for the container to run the collector.py script
+CMD ["python", "/app/collector.py"]
+
---
+++ collector.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import time
+from typing import List
+import traceback
+from datetime import datetime
+from ccapi import EventHandler, SessionOptions, SessionConfigs, Session, Subscription, Event, SubscriptionList
+from dateutil.parser import parse
+import pyarrow as pa
+from pyarrow import dataset as ds
+
+import pyarrow.parquet as pq
+from pyarrow.fs import S3FileSystem
+
+def process_events(events: List[Event], data_dir: str, schema: pa.Schema, exchange_market_pairs: List[tuple]):
+    try:
+        data_buffers = {pair: {col_name: [] for col_name in schema.names} for pair in exchange_market_pairs}
+
+        for event in events:
+            if event.getType() == Event.Type_SUBSCRIPTION_DATA:
+                for message in event.getMessageList():
+                    exchange, market = message.getCorrelationIdList()[0].split(',')
+                    timestamp_start = parse(message.getTimeISO())
+
+                    bid_element, ask_element = message.getElementList()
+                    bid_elementNameValueMap = bid_element.getNameValueMap()
+                    ask_elementNameValueMap = ask_element.getNameValueMap()
+
+                    buffer = data_buffers[(exchange, market)]
+                    buffer['timestamp'].append(int(timestamp_start.timestamp() * 1e9))
+                    buffer['bid_price'].append(bid_element.getValue('BID_PRICE'))
+                    buffer['ask_price'].append(ask_element.getValue('ASK_PRICE'))
+                    buffer['bid_size'].append(bid_element.getValue('BID_SIZE'))
+                    buffer['ask_size'].append(ask_element.getValue('ASK_SIZE'))
+
+        for (exchange, market) in exchange_market_pairs:
+            data_buffer_arrays = {k: pa.array(v) for k, v in data_buffers[(exchange, market)].items()}
+            record_batch = pa.RecordBatch.from_arrays(list(data_buffer_arrays.values()), schema=schema)
+            table = pa.Table.from_batches([record_batch], schema=schema)
+
+            output_path = os.path.join(data_dir, exchange, market)
+            if data_dir.startswith("s3://"):
+                fs = S3FileSystem()
+                fs.mkdir(output_path, recursive=True)
+            else:
+                os.makedirs(output_path, exist_ok=True)
+
+            first_timestamp = data_buffer_arrays['timestamp'][0].as_py()
+            output_file = os.path.join(output_path, f"{first_timestamp}.parquet")
+
+
+            if data_dir.startswith("s3://"):
+                with fs.open_output_stream(output_file) as out:
+                    pq.write_table(table, out, compression="snappy")
+            else:
+                pq.write_table(table, output_file, compression="snappy")
+
+            print(f"Table written for {exchange}/{market} at {time.time()}")
+            
+    except Exception:
+        print(traceback.format_exc())
+        sys.exit(1)
+
+
+def main():
+    exchanges = os.getenv('EXCHANGES', 'binance-usds-futures').split(',')
+    markets = os.getenv('MARKETS', 'ethusdt,btcusdt,bnbusdt').split(',')
+    collect_interval = int(os.getenv('COLLECT_INTERVAL', 60 * 60))
+    data_dir = os.getenv('DATA_DIR', 'file:///data')
+
+    option = SessionOptions()
+    config = SessionConfigs()
+    session = Session(option, config)
+
+    subscriptionList = SubscriptionList()
+    exchange_market_pairs = []
+    for exchange in exchanges:
+        for market in markets:
+            correlation_id = ','.join([exchange, market])
+            subscription = Subscription(exchange, market.upper(), 'MARKET_DEPTH', 'MARKET_DEPTH_MAX=1', correlation_id)
+            subscriptionList.append(subscription)
+            exchange_market_pairs.append((exchange, market))
+
+    schema = pa.schema([
+       
+        ('timestamp', pa.int64()),
+        ('bid_price', pa.float64()),
+        ('ask_price', pa.float64()),
+        ('bid_size', pa.float64()),
+        ('ask_size', pa.float64()),
+    ])
+
+    session.subscribe(subscriptionList)
+    try:
+        while True:
+            time.sleep(collect_interval)
+            events = session.getEventQueue().purge()
+            process_events(events, data_dir, schema, exchange_market_pairs)
+    except KeyboardInterrupt:
+        session.stop()
+        print('Bye')
+
+
+if __name__ == '__main__':
+    main()
+
